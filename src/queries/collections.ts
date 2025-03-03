287a1e7e2e5bc53cebe6c9b5f5f169a66d03ff22
moosernator <white22rg@outlook.com>
2025-03-03 15:18:26 +0000

    Investigate caching and batching

--- src/queries/collections.ts
+++ src/queries/collections.ts
@@ -1,6 +1,11 @@
 import { createDatabaseConnection } from "../database.js";
-import { Course, CoursesInput, SortOrder } from "../graphql.types.js";
+import { Course } from "../graphql.types.js";
 import { RowDataPacket } from "mysql2";
+
+// Due to time constraints and design decisions, I have not implemented
+// another collections table (instead having collection as a course attribute).
+// If I did, I would use a dataloader for this query (batch initial
+// collections query and subsequent queries to get courses in each collection)
 
 export const collections = async () => {
     const connection = await createDatabaseConnection();
